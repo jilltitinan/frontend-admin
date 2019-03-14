5e860d2b77ca98331d9ca4f266caaf5262146cdd
jilltitinan <jilltitinan@gmail.com>
2019-03-14 22:40:48 +0700

    alert error not done yet

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,8 @@ import Overview from './Overview';
 import Insight from './Insight';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
-import GoogleLogin from 'react-google-login';
+// import GoogleLogin from 'react-google-login';
+import { GoogleLogout } from 'react-google-login';
 
 import { connect } from 'react-redux';
 import { login } from '../actions';
@@ -56,14 +57,21 @@ class Header extends Component {
     this.setState({ modalIsOpen: false });
   }
 
+  responseLogout() {
+    // console.log("respen logout : ", response)
+    // this.setState({ modalIsOpen: false });
+    browserHistory.push('/')
+
+  }
+
   render() {
 
     // const { id_reserve, code, status} = this.props.data.login;
 
     console.log(this.props.location.pathname);
 
-    console.log("result : ",this.props.data);
-    
+    console.log("result : ", this.props.data);
+
     return (
       <div className="App">
         <div className='greenHeader' />
@@ -72,7 +80,7 @@ class Header extends Component {
         <div className='grayBorder'>
           <img src={kendal} height="105" width="105" />
           <h1>{this.props.data.w3.ig} </h1>
-          <h2>ID : 555555</h2>
+          <h2>{this.props.data.w3.Eea}</h2>
 
           <ul className='sidebar'>
             <li onClick={() => this.setState({ selected: 'activity2' })}><Link to='/activity' className={this.state.selected === 'activity2' ? 'active' : 'a:hover:not(.active)'} >Activity</Link></li>
@@ -93,8 +101,10 @@ class Header extends Component {
           >
             <div className='modalLogout'>
               <h2 ref={subtitle => this.subtitle = subtitle}>Are you sure to logout?</h2>
-              <button className='modalYes'>Yes</button>
-              <button className='modalNo' onClick={this.closeModal}>No</button>
+              <div className='loginout' >
+                <button className='modalYes' onClick={this.responseLogout}>Yes</button>
+                <button className='modalNo' onClick={this.closeModal}>No</button>
+              </div>
             </div>
 
           </Modal>
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,22 +4,67 @@ import { Link, Router, Route, Redirect, browserHistory } from 'react-router';
 import GoogleLogin from 'react-google-login';
 import { login } from '../actions';
 import { connect } from 'react-redux';
-
-
-
-// const responseGoogle = (response) => {
-    
-//     console.log("google response :", response);
-//     this.props.login(response);
-// }
+import axios from 'axios';
 
 class Login extends Component {
 
-    responseGoogle(response) {
-        console.log("google response :", response);
-        this.props.login(response);
-        browserHistory.push('/activity')
+    constructor(props) {
+        super(props);
+        this.state = {
+            reserve: [],
+
+        }
     }
+    responseGoogle(response) {
+        (async () => {
+            let apiRes = null;
+            try {
+                apiRes = await axios.get(`https://locker54.azurewebsites.net/api/Account/AdminAccount?id_account=${response.El}`);
+            } catch (err) {
+                console.error("Error response:");
+                console.error(err.response.data);    // ***
+                console.error(err.response.status);  // ***
+                console.error(err.response.headers); // ***
+                this.successShow(err);
+                
+
+            } finally {
+                
+                console.log("finally :     ", apiRes);
+                this.props.login(response);
+                browserHistory.push('/activity')
+            }
+        })();
+
+        
+
+        // console.log("google response :", response);
+        // this.props.login(response);
+        // browserHistory.push('/activity')
+
+        // axios.get(`https://locker54.azurewebsites.net/api/Account/AdminAccount?id_account=${response.El}`)
+        //     .then(res => {
+        //         this.setState({ reserve: res.data });
+        //         console.log("id admin : ", res.data)
+        //     })
+    }
+
+    successShow(err) {
+        this.setState({
+            person: err.data
+        });
+        
+    }
+
+    responseGoogleFail(response) {
+        console.log("google response :", response);
+
+    }
+
+    componentDidMount() {
+
+    }
+
 
     render() {
 
@@ -32,7 +77,7 @@ class Login extends Component {
                         clientId="367051335006-3ibce4jslddaibdincs9t267vmpgjkob.apps.googleusercontent.com"
                         buttonText="Login"
                         onSuccess={this.responseGoogle.bind(this)}
-                        onFailure={(response) => this.responseGoogle(response)}
+                        onFailure={(response) => this.responseGoogleFail(response)}
                         redirectUri='http://localhost:3000/home'
                     />
                 </div>
