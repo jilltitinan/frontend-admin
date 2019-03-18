60a5af88cee6c37cc5e177af8e97c044f4e1649b
jilltitinan <jilltitinan@gmail.com>
2019-03-18 10:31:34 +0700

    no noti when fail to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,24 +19,28 @@ class Login extends Component {
         (async () => {
             let apiRes = null;
             try {
-                apiRes = await axios.get(`https://locker54.azurewebsites.net/api/Account/AdminAccount?id_account=${response.El}`);
+                apiRes = await axios.get(`https://locker54.azurewebsites.net/web/IsAdmin?accountID=${response.El}`)
+                    .then(res => {
+                        this.setState({ reserve: res.data });
+                        console.log("id admin : ", res.data)
+                        this.props.login(response);
+                        browserHistory.push('/activity')
+                    })
             } catch (err) {
                 console.error("Error response:");
                 console.error(err.response.data);    // ***
                 console.error(err.response.status);  // ***
                 console.error(err.response.headers); // ***
                 this.successShow(err);
-                
-
-            } finally {
-                
-                console.log("finally :     ", apiRes);
-                this.props.login(response);
-                browserHistory.push('/activity')
             }
+            // finally {
+            //     console.log("finally :     ", apiRes);
+            //     this.props.login(response);
+            //     browserHistory.push('/activity')
+            // }
         })();
 
-        
+
 
         // console.log("google response :", response);
         // this.props.login(response);
@@ -53,7 +57,7 @@ class Login extends Component {
         this.setState({
             person: err.data
         });
-        
+
     }
 
     responseGoogleFail(response) {
@@ -71,11 +75,13 @@ class Login extends Component {
         return (
             <div className="App">
                 <div className='login'>
+                    <br /><br />
                     <img src={logo} />
-                    <br />
+                    <br /><br />
+                    <h1 className='quoteWhite'>Locker mangement system on a mobile application</h1>
                     <GoogleLogin
                         clientId="367051335006-3ibce4jslddaibdincs9t267vmpgjkob.apps.googleusercontent.com"
-                        buttonText="Login"
+                        buttonText="Login with Google account"
                         onSuccess={this.responseGoogle.bind(this)}
                         onFailure={(response) => this.responseGoogleFail(response)}
                         redirectUri='http://localhost:3000/home'
