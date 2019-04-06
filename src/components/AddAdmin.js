3278be6af1e95ffec1d90bceed90525e45b0d0ac
jilltitinan <jilltitinan@gmail.com>
2019-04-06 22:56:42 +0700

    css add admin not done yet

--- src/components/AddAdmin.js
+++ src/components/AddAdmin.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import _ from 'lodash';
-import moment from 'moment';
-import { Link } from "react-router";
 
 class AddAdmin extends Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
+        this.state = { name: '', email: ''};
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleEmail = this.handleEmail.bind(this);
+
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ value: event.target.value });
+        this.setState({ name: event.target.value });
     }
+
+    
+    handleEmail(event) {
+        this.setState({ email: event.target.value });
+    }
+
 
     handleSubmit(event) {
         alert('A name was submitted: ' + this.state.value);
@@ -29,19 +35,21 @@ class AddAdmin extends Component {
         return (
             <div className="main-display">
                 <p className="header-text"><b>ADMINISTRATOR</b></p>
+                <div className='field'>
+                    <form onSubmit={this.handleSubmit} >
+                        <label>
+                            Name - Surname:
+          <input type="text" value={this.state.name} onChange={this.handleChange} />
+                        </label>
+                        <br></br>
+                        <label>
+                            Email:
+          <input type="text" value={this.state.email} onChange={this.handleEmail} />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
 
-                <form onSubmit={this.handleSubmit} >
-                    <label>
-                        Name - Surname:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-                    </label>
-                    <br></br>
-                    <label>
-                        Email:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
 
             </div>
         );
